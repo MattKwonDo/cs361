ab445a245dc1e265d767902559275cddad6db266
childrem <38900005+childrem@users.noreply.github.com>
2019-05-28 16:18:56 -0700

    Added Basic JS Server Code

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
 
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+//app.use('/branch', require('./branch.js'));
+//app.use('/department', require('./department.js'));
+//app.use('/doctor', require('./doctor.js'));
+//app.use('/doctor_branch', require('./doctor_branch.js'));
+//app.use('/doctor_department', require('./doctor_department.js'));
+//app.use('/patient', require('./patient.js'));
+//app.use('/update_patient', require('./update_patient.js'));
+app.use('/', express.static('public'));
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
